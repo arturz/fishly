2047a718e4cd48b9cac578b58da5cb6e4a5a6d24
Artur <26014458+xout@users.noreply.github.com>
2020-03-30 12:04:20 +0200

    Set session lifetime

--- api/User.php
+++ api/User.php
@@ -1,5 +1,8 @@
 <?php
   include_once 'utils.php';
+  //7 days
+  ini_set('session.gc_maxlifetime', 604800);
+  ini_set('session.cookie_lifetime', 604800);
   session_start();
 
   abstract class UserStatuses {

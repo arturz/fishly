7013f227dbc64940681709bbd5e889c87feb2231
Artur <26014458+xout@users.noreply.github.com>
2020-06-04 11:31:47 +0200

    Shuffle words

--- app/src/components/SetPage/SetPageContainer.tsx
+++ app/src/components/SetPage/SetPageContainer.tsx
@@ -4,6 +4,14 @@ import { useParams } from 'react-router-dom'
 import getSet from '../../api/set/getSet'
 import SetPage from './SetPage'
 
+function shuffle(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
+
 export default () => {
   const [set, setSet] = useState(null)
   const { id } = useParams()
@@ -12,7 +20,12 @@ export default () => {
     if(!id)
       return
     
-    getSet(parseInt(id)).then(setSet)
+    getSet(parseInt(id)).then(fetchedSet => {
+      setSet({
+        words: shuffle(fetchedSet.words),
+        ...fetchedSet
+      })
+    })
   }, [id])
 
   return <SetPage id={id} set={set} setSet={setSet} />
